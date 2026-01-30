3884d75b9c46457af2054f56f3fedf92b3762a3a
Yngwie <patricio2321453@mkt.ceu.edu.ph>
2026-01-30 20:49:55 +0800

    osrm  walking route from point a to point b (click 2 points on the map)

--- src/features/map/Map.tsx
+++ src/features/map/Map.tsx
@@ -5,14 +5,71 @@ function Map() {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const mapInstance = useRef<maplibregl.Map | null>(null);
 
+  async function getRoute(start: [number, number], end: [number, number]) {
+  const url = `http://localhost:5000/route/v1/foot/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&overview=full`;
+
+  const res = await fetch (url);
+  const data = await res.json();
+  const coordinates = data.routes[0].geometry.coordinates;
+
+  const source = mapInstance.current!.getSource("route") as maplibregl.GeoJSONSource;
+
+  source.setData({
+    type: "Feature",
+    properties: {},
+    geometry: { 
+      type: "LineString",
+      coordinates: coordinates  
+    },
+  })
+}
+
   useEffect(() => {
     if (mapContainer.current && !mapInstance.current) {
       mapInstance.current = new maplibregl.Map({
         container: mapContainer.current,
-        style: "positron.json",
-        center: [0, 0],
-        zoom: 2,
+        style: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json", //working tile server
+        center: [121.0, 12.5], //starting point Philippines
+        zoom: 5,
       });
+
+
+      let points: [number, number][] = [];
+
+      mapInstance.current.on("click", (e) => {  
+        points.push([e.lngLat.lng, e.lngLat.lat]);
+
+         if (points.length === 2) {
+        getRoute(points[0], points[1]);
+        points = [];
+      }   
+      });
+
+        
+    mapInstance.current.on("load", () => {
+      mapInstance.current!.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: [],
+          }
+        }
+      });
+    
+
+    mapInstance.current!.addLayer({
+      id: "route-line",
+      type: "line",
+      source: "route",
+      paint: {
+        "line-color": "#2563eb",
+        "line-width": 4,
+      },
+    });
+  });
     }
 
     return () => {
@@ -23,7 +80,8 @@ function Map() {
     };
   }, []);
 
-  return <div ref={mapContainer} className="" />;
+  return <div ref={mapContainer} style={{width: '100%', height: '100vh'}} className="" />;
 }
 
-export default Map;
+export default Map
+
